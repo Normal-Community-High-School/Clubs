eba0b6208a9f248332124bb138d72f80230e7ed0
Nick Cooper <nicholascullencooper@gmail.com>
2021-11-04 19:24:22 -0500

    fix static image issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
+const withOptimizedImages = require('next-optimized-images');
 
-module.exports = {
+module.exports =  withOptimizedImages({
   assetPrefix: isProd ? '/clubs/' : ''
-}
+})
 
 
