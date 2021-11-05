89913e66b0f351b51e944e8f8ecb75cc72f3b881
Nick Cooper <nicholascullencooper@gmail.com>
2021-11-04 19:30:13 -0500

    ok NO images at all

--- pages/index.js
+++ pages/index.js
@@ -57,10 +57,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src={require('./public/vercel.svg')} width={72} height={16} />
-          </span>
         </a>
       </footer>
     </div>
