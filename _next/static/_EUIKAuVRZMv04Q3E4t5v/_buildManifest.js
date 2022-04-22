aab726661af1b24c26d318fcf89989ae703f7f42
NicholasCullenCooper <NicholasCullenCooper@users.noreply.github.com>
2022-04-22 12:49:12 +0000

    deploy: 6769c840ac11f67366fb56f5e99f6b14592bf553

---
+++ _next/static/_EUIKAuVRZMv04Q3E4t5v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/94cccec734373777.css","static/chunks/pages/index-bc562fe49ad8435a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
